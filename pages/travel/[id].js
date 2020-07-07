45529e1a492849871502cc6dfcf4e41440730d30
Jukka Suokas <jukka.suokas@reaktor.com>
2020-07-07 16:45:40 +0300

    Fix spacing on story screen

--- pages/travel/[id].js
+++ pages/travel/[id].js
@@ -80,14 +80,16 @@ function Story({ story }) {
           <div className={css.storyBody}>
             <ReactMarkdown source={story.content.markdown} />
             {story.youtubeUrl && (
-              <iframe
-                width="100%"
-                height="400"
-                src={story.youtubeUrl}
-                frameBorder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
+              <p>
+                <iframe
+                  width="100%"
+                  height="400"
+                  src={story.youtubeUrl}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </p>
             )}
           </div>
         </motion.div>
